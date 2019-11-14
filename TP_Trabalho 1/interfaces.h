e123ece5d055dc5092a6bc69375ced858c135a40
Capelaum <thecapellett@gmail.com>
2019-11-14 10:16:56 -0300

    Classes de interfaces adicionadas

--- TP_Trabalho 1/interfaces.h
+++ TP_Trabalho 1/interfaces.h
@@ -6,49 +6,62 @@
 
 using namespace std;
 
-// Declarações de serviços na camada de apresentação
-// -----------------------------------------------------------------
-
-// Declaração de interface para serviço de autenticação na camada de apresentação.
+// Declarações de Interfaces na Camada de Apresentação
+// --------------------------------------------------------------------------------------
+// Declaração de interface para funcionalidades de Autenticação na camada de apresentação.
 
 class IAutenticacao
 {
 public:
 
     // Método por meio do qual é solicitado autenticação.
-    virtual ResultadoAutenticacao Autenticar() throw(runtime_error) = 0;
+    bool Autenticar() throw(runtime_error) = 0;
 
     // Método destrutor virtual.
-    virtual ~IUAutenticacao(){}
+    ~IAutenticacao(){}
 };
-
-// Declaração de interface para serviço de usuário na camada de apresentação.
+// --------------------------------------------------------------------------------------
+// Declaração de interface para funcionalidades de Usuário na camada de apresentação.
 
 class IUsuario
 {
 public:
 
-    // Método por meio do qual é solicitado cadastro.
-    virtual ResultadoCadastro cadastrar() throw(runtime_error) = 0;
+    // Métodos relacionados a contas de Usuário
+    virtual Cadastrar() throw(runtime_error) = 0;
+    virtual Editar()    throw(runtime_error) = 0;
+    virtual Excluir()   throw(runtime_error) = 0;
+    virtual Pesquisar() throw(runtime_error) = 0;
 
     // Método destrutor virtual.
     virtual ~IUsuario(){}
 };
-
-// Declaração de interface para serviço de usuário na camada de apresentação.
+// --------------------------------------------------------------------------------------
+// Declaração de interface para funcionalidades de Caronas na camada de apresentação.
 
 class ICarona
 {
 public:
 
     // Método por meio do qual é solicitado cadastro.
-    virtual ResultadoCadastro cadastrar() throw(runtime_error) = 0;
+    virtual cadastrar() throw(runtime_error) = 0;
 
     // Método destrutor virtual.
-    virtual ~IUsuario(){}
+    virtual ~ICarona(){}
 };
+// --------------------------------------------------------------------------------------
+// Declaração de interface para funcionalidades de Reservas na camada de apresentação.
 
+class IReserva
+{
+public:
 
+    // Método por meio do qual é solicitado cadastro.
+    virtual cadastrar() throw(runtime_error) = 0;
+
+    // Método destrutor virtual.
+    virtual ~IReserva(){}
+};
 
 
 #endif // INTERFACES_H_INCLUDED
